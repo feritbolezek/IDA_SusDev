e0496c0f5b95a93a728dbe8dd63f4b1fa2114168
feritbolezek <feritbolezek@hotmail.com>
2018-11-21 20:46:49 +0100

    choices look better designwise now.

--- js/Quiz.js
+++ js/Quiz.js
@@ -1,9 +1,11 @@
 var arrivedFromCourse = false;
 
+var lastChoice = null;
+
 // Checking from where we came. If it was from a course,
 // then we can start displaying the quiz for it.
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     if (document.location.href.includes("?")) {
         // Great!
@@ -17,4 +19,48 @@ $(document).ready(function() {
         $('.rightBar').removeClass("col-2");
         $('.rightBar').addClass("col-3");
     }
-    });
+});
+
+
+
+
+
+var points = 0;
+var maxPoints;
+
+var questions = [{
+        question: "In what city was Donald Trump born?",
+        choice1: "Los Angeles",
+        choice2: "New York City",
+        choice3: "Chicago",
+        choice4: "Toronto",
+        answer: "New York City"
+    },
+    {
+        question: "In what city was Ferit born?",
+        choice1: "Istanbul",
+        choice2: "Helsingborg",
+        choice3: "Malmö",
+        choice4: "Jakarta",
+        answer: "Malmö"
+    }
+];
+
+function extractData() {
+    console.log(questions[1].question);
+}
+
+window.onload = function () {
+    extractData();
+}
+
+// Just code being executed based on which answer the user chose.
+function selectionWasMade(selection) {
+    if (selection.id === lastChoice) return;
+    if (lastChoice) {
+        console.log("Setting! " + lastChoice);
+        $("#"+lastChoice).removeClass("choiceSelected");
+    }
+    
+    lastChoice = selection.id;
+}
